8a08ef6df774597a50b4e82f9f927c2fc8e745fc
larspLd <71321296+larspLd@users.noreply.github.com>
2023-06-29 20:03:36 +0300

    Added license information

---
+++ build/CMakeFiles/SDL2Test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/lasse/dev/SDL2-Test/src/Entity.cpp" "CMakeFiles/SDL2Test.dir/src/Entity.cpp.obj" "gcc" "CMakeFiles/SDL2Test.dir/src/Entity.cpp.obj.d"
+  "C:/Users/lasse/dev/SDL2-Test/src/Graphics/RendererBase.cpp" "CMakeFiles/SDL2Test.dir/src/Graphics/RendererBase.cpp.obj" "gcc" "CMakeFiles/SDL2Test.dir/src/Graphics/RendererBase.cpp.obj.d"
+  "C:/Users/lasse/dev/SDL2-Test/src/Graphics/SDLRenderer.cpp" "CMakeFiles/SDL2Test.dir/src/Graphics/SDLRenderer.cpp.obj" "gcc" "CMakeFiles/SDL2Test.dir/src/Graphics/SDLRenderer.cpp.obj.d"
+  "C:/Users/lasse/dev/SDL2-Test/src/Game.cpp" "CMakeFiles/SDL2Test.dir/src/game.cpp.obj" "gcc" "CMakeFiles/SDL2Test.dir/src/game.cpp.obj.d"
+  "C:/Users/lasse/dev/SDL2-Test/src/Main.cpp" "CMakeFiles/SDL2Test.dir/src/main.cpp.obj" "gcc" "CMakeFiles/SDL2Test.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
