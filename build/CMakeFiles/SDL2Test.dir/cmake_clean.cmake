59ff7390d25e321b41d6598dc3f536dd2a5d3c95
larspLd <71321296+larspLd@users.noreply.github.com>
2023-06-29 20:12:53 +0300

    .gitignore fix tests

---
+++ build/CMakeFiles/SDL2Test.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "C:/Users/lasse/dev/SDL2-Test/bin/Debug/SDL2Test.exe"
+  "C:/Users/lasse/dev/SDL2-Test/bin/Debug/SDL2Test.exe.manifest"
+  "C:/Users/lasse/dev/SDL2-Test/bin/Debug/SDL2Test.pdb"
+  "C:/Users/lasse/dev/SDL2-Test/bin/Debug/libSDL2Test.dll.a"
+  "CMakeFiles/SDL2Test.dir/src/Entity.cpp.obj"
+  "CMakeFiles/SDL2Test.dir/src/Entity.cpp.obj.d"
+  "CMakeFiles/SDL2Test.dir/src/Game.cpp.obj"
+  "CMakeFiles/SDL2Test.dir/src/Game.cpp.obj.d"
+  "CMakeFiles/SDL2Test.dir/src/Graphics/RendererBase.cpp.obj"
+  "CMakeFiles/SDL2Test.dir/src/Graphics/RendererBase.cpp.obj.d"
+  "CMakeFiles/SDL2Test.dir/src/Graphics/SDLRenderer.cpp.obj"
+  "CMakeFiles/SDL2Test.dir/src/Graphics/SDLRenderer.cpp.obj.d"
+  "CMakeFiles/SDL2Test.dir/src/Main.cpp.obj"
+  "CMakeFiles/SDL2Test.dir/src/Main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL2Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
