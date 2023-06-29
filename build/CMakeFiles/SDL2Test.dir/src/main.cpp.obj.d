e8bee522677a9771388d65cd067e944921a7b763
larspLd <71321296+larspLd@users.noreply.github.com>
2023-06-29 20:37:12 +0300

    tests

---
+++ build/CMakeFiles/SDL2Test.dir/src/main.cpp.obj.d
@@ -0,0 +1,80 @@
+CMakeFiles/SDL2Test.dir/src/main.cpp.obj: \
+ C:\Users\lasse\dev\SDL2-Test\src\Main.cpp \
+ C:/Users/lasse/dev/SDL2-Test/include/Graphics/SDLRenderer.h \
+ c:\mingw\include\sdl2\sdl.h c:\mingw\include\sdl2\SDL_main.h \
+ c:\mingw\include\sdl2\SDL_stdinc.h c:\mingw\include\sdl2\SDL_config.h \
+ c:\mingw\include\sdl2\SDL_platform.h c:\mingw\include\sdl2\begin_code.h \
+ c:\mingw\include\sdl2\close_code.h c:\mingw\include\sdkddkver.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stdint.h \
+ c:\mingw\include\stdint.h c:\mingw\include\_mingw.h \
+ c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
+ c:\mingw\include\sdl2\SDL_assert.h c:\mingw\include\sdl2\SDL_atomic.h \
+ c:\mingw\include\sdl2\SDL_audio.h c:\mingw\include\sdl2\SDL_error.h \
+ c:\mingw\include\sdl2\SDL_endian.h c:\mingw\include\sdl2\SDL_mutex.h \
+ c:\mingw\include\sdl2\SDL_thread.h c:\mingw\include\process.h \
+ c:\mingw\include\sys\types.h c:\mingw\include\stdint.h \
+ c:\mingw\include\sdl2\SDL_rwops.h c:\mingw\include\sdl2\SDL_clipboard.h \
+ c:\mingw\include\sdl2\SDL_cpuinfo.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\immintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\mmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\xmmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\mm_malloc.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\stdlib.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\cstdlib \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\mingw32\bits\c++config.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\mingw32\bits\os_defines.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\mingw32\bits\cpu_defines.h \
+ c:\mingw\include\stdlib.h c:\mingw\include\errno.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\emmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\pmmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\tmmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\smmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\popcntintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\wmmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avxintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx2intrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512fintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512erintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512pfintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512cdintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vlintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512bwintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512dqintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vlbwintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vldqintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512ifmaintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512ifmavlintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vbmiintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vbmivlintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\shaintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\lzcntintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\bmiintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\bmi2intrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\fmaintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\f16cintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\rtmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\xtestintrin.h \
+ c:\mingw\include\sdl2\SDL_events.h c:\mingw\include\sdl2\SDL_video.h \
+ c:\mingw\include\sdl2\SDL_pixels.h c:\mingw\include\sdl2\SDL_rect.h \
+ c:\mingw\include\sdl2\SDL_surface.h \
+ c:\mingw\include\sdl2\SDL_blendmode.h \
+ c:\mingw\include\sdl2\SDL_keyboard.h c:\mingw\include\sdl2\SDL_keycode.h \
+ c:\mingw\include\sdl2\SDL_scancode.h c:\mingw\include\sdl2\SDL_mouse.h \
+ c:\mingw\include\sdl2\SDL_joystick.h c:\mingw\include\sdl2\SDL_guid.h \
+ c:\mingw\include\sdl2\SDL_gamecontroller.h \
+ c:\mingw\include\sdl2\SDL_sensor.h c:\mingw\include\sdl2\SDL_quit.h \
+ c:\mingw\include\sdl2\SDL_gesture.h c:\mingw\include\sdl2\SDL_touch.h \
+ c:\mingw\include\sdl2\SDL_filesystem.h \
+ c:\mingw\include\sdl2\SDL_haptic.h c:\mingw\include\sdl2\SDL_hidapi.h \
+ c:\mingw\include\sdl2\SDL_hints.h c:\mingw\include\sdl2\SDL_loadso.h \
+ c:\mingw\include\sdl2\SDL_log.h c:\mingw\include\sdl2\SDL_messagebox.h \
+ c:\mingw\include\sdl2\SDL_metal.h c:\mingw\include\sdl2\SDL_power.h \
+ c:\mingw\include\sdl2\SDL_render.h c:\mingw\include\sdl2\SDL_shape.h \
+ c:\mingw\include\sdl2\SDL_system.h c:\mingw\include\sdl2\SDL_timer.h \
+ c:\mingw\include\sdl2\SDL_version.h c:\mingw\include\sdl2\SDL_locale.h \
+ c:\mingw\include\sdl2\SDL_misc.h \
+ C:/Users/lasse/dev/SDL2-Test/include/Graphics/RendererBase.h \
+ C:/Users/lasse/dev/SDL2-Test/include/Game.h \
+ C:/Users/lasse/dev/SDL2-Test/include/Graphics/RendererBase.h
